a5fe8e7b7ecb5d0e1ee2b8219ebdfdbbb3515bcc
hjm8560 <gjwjdals96@naver.com>
2022-03-15 20:13:02 +0900

    2주차

---
+++ 02/app.js
@@ -0,0 +1,64 @@
+/**
+ * 02주차 실습
+ * 
+ * 
+ */
+var name1 = 'aj';
+name1 = 111;
+// 이름을 찍는다
+console.log(name1);
+
+
+var isMan = 'true';
+var isMan1 = true;
+console.log(isMan);
+console.log(isMan1);
+
+function log(str) {
+    console.log(str);
+}
+
+log('hello');
+
+// object  
+// var img = {};
+// img.width = 100;
+// img.height = 200;
+// console.log(img);
+
+// var arr =[1, 2, 3, 4, 5];
+
+// console.log(arr)
+
+// console.log(1+1);
+// console.log(name + 'hello');
+
+// var me = {
+//     age : 11,
+//     name: 'aj',
+//     isMan : true,
+//     sayHello : function() {
+//         console.log('hello');
+//     },
+// };
+
+// me.sayHello();
+
+// var arr = [1, 2, 3, 4, 5];
+// console.log(arr.length)
+
+// var todos = ['운동'];
+
+// var todo = '게임';
+// todos.push(todo);
+
+// todos.forEach(function (todo)) {
+//     console.log(todo);
+// };
+
+var age = 20;
+
+var isBool = true;
+
+var month = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
---
+++ 02/node1.js
@@ -0,0 +1,4 @@
+var name1 = 'aj';
+console.log(name1);
+var date = new Date();
+date.
